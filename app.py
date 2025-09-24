3b6b53f901a1ea9dd9e0df950cae00b8ebd42225
Brenno Pacheco <brenno_pacheco@hotmail.com>
2025-09-23 21:24:38 -0300

    Construindo o Gráfico de Receita Mensal

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly as px
 from dataset import df
 from utils import format_number
-from graficos import grafico_map_estado
+from graficos import grafico_map_estado, grafico_rec_mensal
 
 st.set_page_config(layout='wide')
 
@@ -21,5 +21,6 @@ with aba2:
         st.plotly_chart(grafico_map_estado, use_container_width=True)
     with coluna2:
         st.metric('Quantidade de Vendas', format_number(df.shape[0]))
+        st.plotly_chart(grafico_rec_mensal, use_container_width=True)
 
     
