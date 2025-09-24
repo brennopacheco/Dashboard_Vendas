1fa5da7c5a4ed8ef51289aebc1c9c666678cfff7
Brenno Pacheco <brenno_pacheco@hotmail.com>
2025-09-23 21:17:48 -0300

    Construindo a Tabela de Receita Mensal

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from dataset import df
+import pandas as pd
 
 def format_number(value, prefix=''):
     for unit in ['', 'mil']:
@@ -8,7 +9,7 @@ def format_number(value, prefix=''):
     return f'{prefix} {value:.2f} milhões'
 
 
-
+# 1 - Dataframe Receita por Estado
 df_rec_estado = df.groupby('Local da compra')[['Preço']].sum()
 
 df_rec_estado = df.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']] \
@@ -16,4 +17,13 @@ df_rec_estado = df.drop_duplicates(subset='Local da compra')[['Local da compra',
     .sort_values('Preço', ascending=False)
 
 
-print(df_rec_estado)
+# print(df_rec_estado)
+
+# 2 - Receita Mensal
+df_rec_mensal = df.set_index('Data da Compra') \
+    .groupby(pd.Grouper(freq='M'))[['Preço']].sum() \
+    .reset_index()
+
+df_rec_mensal['Ano'] = df_rec_mensal['Data da Compra'].dt.year # Coletando o ano da coluna data
+df_rec_mensal['Mes'] = df_rec_mensal['Data da Compra'].dt.month_name() # Coletando o mês da coluna data
+print(df_rec_mensal)
